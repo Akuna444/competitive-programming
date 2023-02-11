95c6005a388892049af9e46356d3b44e7b0d2fac
Aklil Solomon <56131204+Akuna444@users.noreply.github.com>
2023-02-11 13:38:16 +0300

    Create reverse_linked_list.py

---
+++ reverse_linked_list.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        # Initialize prev pointer as NULL...
+        prev = None
+        # Initialize the curr pointer as the head...
+        curr = head
+        # Run a loop till curr points to NULL...
+        while curr:
+            # Initialize next pointer as the next pointer of curr...
+            next = curr.next
+            # Now assign the prev pointer to curr’s next pointer.
+            curr.next = prev
+            # Assign curr to prev, next to curr...
+            prev = curr
+            curr = next
+        return prev       # Return the prev pointer to get the reverse linked list...
